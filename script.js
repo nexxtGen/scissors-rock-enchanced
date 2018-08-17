afab4a1efd1de4bbed28d9413728e472ed117870
Kamil Szczypior <kszczypior1@gmail.com>
2018-08-17 12:59:23 +0200

    Fix code

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 'use strict';
 
+        /*Vars*/         
+        var output = document.getElementById('first-output');
+        var output2 = document.getElementById('result');            
+        var startButton = document.getElementById('start-button');  
+        var playableButtons = document.getElementsByClassName('player-move');        
+        var outputPlayer = document.getElementById('player-score');
+        var outputComputer = document.getElementById('computer-score');
+        var outputLevelCap = document.getElementById('level-cap');  
+        var outputRoundsPlayed = document.getElementById('rounds-played'); 
+        var outputModalContent = document.getElementById('for-table');
+        var outputModalContent2 = document.getElementById('for-table2'); 
+        var params = {userWinRate: 0, 
+                        computerWinRate: 0, 
+                        roundsPlayed: 0, 
+                        levelCap: 0,
+                        progress:[]
+                    };
 
-        /*FUNCTIONS*/
+        /*FUNCTIONS*/        
         // Actual move output message.    
         var outputMessageGame = function(wonValue, playerFinal, computerFinal) {               
             output.innerHTML = '<h4>'+'<b>'+ wonValue + '</b>' + ' you played ' + '<b>'+ playerFinal +'</b>'+ ', computer played '+'<b>' + computerFinal +'</b>'+ '<br></h4>';                
-        }
-        
-        // Game basic logic.        
+        }     
 
+        // Game basic logic. 
         var computerChoice = function() {
             var randomNumber = Math.random();
             var computerNumber = parseInt(randomNumber * 3 + 1);
@@ -24,7 +40,7 @@
                return 'scissors';  
             }                              
         }          
-        
+        // Generate table from param array and  push this to one of two modals.
         var generateEndgameTable = function(outputModalElement, showModalId, hideModalId ) {
             for (var i = 0; i < params.progress.length; i++) {
                 var round = params.progress[i].round;
@@ -32,7 +48,7 @@
                 var computerChoice = params.progress[i].computer;
                 var roundWinner = params.progress[i].winnerIs;
                 var roundStatus = params.progress[i].roundStatus;
-                outputModalElement.innerHTML += '<span style="margin: 0 auto">'+'<table style="border: 1px solid black;">' + '<tr>' + '<td>' + round + '</td>' + '<td>' + playerChoice + '</td>' + '<td>' + computerChoice + '</td>'+ '<td>' + roundWinner + '</td>'+ '<td>' + roundStatus + '</td>' + '</tr>' + '</table>'+'</span>';                                              
+                outputModalElement.innerHTML += '<span class="endgame-table">'+'<table>' + '<tr>' + '<td>' + round + '</td>' + '<td>' + playerChoice + '</td>' + '<td>' + computerChoice + '</td>'+ '<td>' + roundWinner + '</td>'+ '<td>' + roundStatus + '</td>' + '</tr>' + '</table>'+'</span>';                                              
            }  
            document.querySelector('#modal-overlay').classList.add('show');
            document.querySelector(showModalId).classList.add('show');
@@ -41,24 +57,22 @@
 
         function handleClick(choice) {
             if (params.userWinRate < params.levelCap && params.computerWinRate < params.levelCap ){                                      
-                palyerMove(choice);                
+                playerMove(choice);                
                 outputPlayer.innerHTML = params.userWinRate;
                 outputComputer.innerHTML = params.computerWinRate;
                 outputRoundsPlayed.innerHTML = '<h2> Move count: '+params.roundsPlayed+'</h2>';
            }      
            else if (params.userWinRate == params.levelCap && params.computerWinRate < params.levelCap) { 
-                    outputModalContent.innerHTML += '<span style="margin: 0 auto; font-weight: bold;">'+'<table style="border: 1px solid black;">' + '<tr>' + '<td>' + 'round' + '</td>' + '<td>' + 'player move' + '</td>' + '<td>' + 'computer move' + '</td>'+ '<td>' + 'win/lost' + '</td>'+ '<td>' + 'game status' + '</td>' + '</tr>' + '</table>'+'</span>';                       
-                    generateEndgameTable(outputModalContent, '#modal-one', '#modal-two');
-                    //stopPropagation;
+                    outputModalContent.innerHTML += '<span class="output-modal">'+'<table>' + '<tr>' + '<td>' + 'round' + '</td>' + '<td>' + 'player move' + '</td>' + '<td>' + 'computer move' + '</td>'+ '<td>' + 'win/lost' + '</td>'+ '<td>' + 'game status' + '</td>' + '</tr>' + '</table>'+'</span>';                       
+                    generateEndgameTable(outputModalContent, '#modal-one', '#modal-two');                    
            }
            else if (params.computerWinRate == params.levelCap && params.userWinRate < params.levelCap) { 
-                    outputModalContent2.innerHTML += '<span style="margin: 0 auto; font-weight: bold;">'+'<table style="border: 1px solid black;">' + '<tr>' + '<td>' + 'round' + '</td>' + '<td>' + 'player move' + '</td>' + '<td>' + 'computer move' + '</td>'+ '<td>' + 'win/lost' + '</td>'+ '<td>' + 'game status' + '</td>' + '</tr>' + '</table>'+'</span>';
-                    generateEndgameTable(outputModalContent2, '#modal-two', '#modal-one');
-                    //stopPropagation;
+                    outputModalContent2.innerHTML += '<span class="output-modal">'+'<table>' + '<tr>' + '<td>' + 'round' + '</td>' + '<td>' + 'player move' + '</td>' + '<td>' + 'computer move' + '</td>'+ '<td>' + 'win/lost' + '</td>'+ '<td>' + 'game status' + '</td>' + '</tr>' + '</table>'+'</span>';
+                    generateEndgameTable(outputModalContent2, '#modal-two', '#modal-one');                    
             }                                  
         }        
 
-        var palyerMove = function(playerFinal){ 
+        var playerMove = function(playerFinal){ 
             var won = 'YOU WON!:'; 
             var lost = 'YOU LOST :(';
             var draw = 'IS DEAD-HEAD:'; 
@@ -91,28 +105,10 @@
                     return params.computerWinRate;  
             }               
         }      
-        /*Functions END*/  
-
-        /*Vars*/         
-        var output = document.getElementById('first-output');
-        var output2 = document.getElementById('result');            
-        var startButton = document.getElementById('start-button');  
-        var playableButtons = document.getElementsByClassName('player-move');        
-        var outputPlayer = document.getElementById('player-score');
-        var outputComputer = document.getElementById('computer-score');
-        var outputLevelCap = document.getElementById('level-cap');  
-        var outputRoundsPlayed = document.getElementById('rounds-played'); 
-        var outputModalContent = document.getElementById('for-table');
-        var outputModalContent2 = document.getElementById('for-table2'); 
-
-        var params = {userWinRate: 0, 
-                        computerWinRate: 0, 
-                        roundsPlayed: 0, 
-                        levelCap: 0,
-                        progress:[]
-                    };
+        /*Functions END*/          
         
         //Rest 
+        // start/restart button logic
         startButton.addEventListener('click', function() {  
             params.progress = [];
             params.userWinRate = 0;
@@ -131,10 +127,9 @@
             }
             else {
                 outputLevelCap.innerHTML = 'Hello stranger, set correct round number! (1-99999)' + '<br><br>';
-            }
-            return params.userWinRate, params.computerWinRate, params.roundsPlayed, params.levelCap; 
+            }            
         });             
-        
+        // Add to all playable buttons function hancleClick with "data-move" param
         for ( var i = 0; i < playableButtons.length; i++ ) {                           
             playableButtons[i].addEventListener('click', function(event) { 
                var valueButton = this.getAttribute("data-move");
@@ -158,12 +153,10 @@
             closeButtons[i].addEventListener('click', hideModal);
         }
         
-        // Overlay click close
-        
+        // Overlay click close        
         document.querySelector('#modal-overlay').addEventListener('click', hideModal);
         
-        // Overlay stop propagination 
-        
+        // Overlay stop propagination         
         var modals = document.querySelectorAll('.modal');
         
         for(var i = 0; i < modals.length; i++){
